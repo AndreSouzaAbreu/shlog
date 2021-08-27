a851e459adcfeff01f17a5e92e42ca313726a560
Andre Souza Abreu <andresouzaabreu.dev@gmail.com>
2021-08-26 22:28:32 -0300

    When updating the CSS, update all HTML files too
    When updating the website's CSS file, the script generates a new CSS
    file in the public directory whose file name includes a md5 hash of the
    file. So, it's necessary to update all HTML files to reflect that
    change.

--- maketemplate.sh
+++ maketemplate.sh
@@ -6,4 +6,6 @@ PUBLIC_DIR=./public
 fhash=$(md5sum ${ASSETS_DIR}/app.css | awk '{ print $1 }')
 find ${PUBLIC_DIR} -maxdepth 1 -type f -iname 'app*.css' -delete
 cp ${ASSETS_DIR}/app.css ${PUBLIC_DIR}/app.${fhash}.css
-sed -i "/href/s/app.*\.css/app.${fhash}.css/" ./template.html
+echo ${fhash}
+find ./template.html ${PUBLIC_DIR} -type f -iname '*.html' \
+  -exec sed -i "/href/s/app.*\.css/app.${fhash}.css/"  '{}' \+
